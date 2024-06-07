b6a593526ef5b06bfdabba21cd732a2f17e6bcfc
kjh <devkjh418@gmail.com>
2024-06-08 02:19:52 +0900

    EDIT: 테스트 코드 정리
    테스트 코드도 코드이기 때문에 유지보수 대상이다.
    테스트 메서드에서 발생하는 중복을 제거한다.

--- kjh418/project/chapter02/src/test/java/chap02/PasswordStrengthMeterTest.java
+++ kjh418/project/chapter02/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -7,29 +7,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("TDD 예: 암호 검사기")
 public class PasswordStrengthMeterTest {
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    private void assertStrength(String password, PasswordStrength expStr){
+        PasswordStrength result = meter.meter(password);
+        assertEquals(expStr,result);
+    }
+
     @DisplayName("[첫 번째 테스트] 모든 규칙을 충족하는 경우")
     @Test
     void meetsAllCriteria_Then_Strong(){
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab12!@AB");
-        assertEquals(PasswordStrength.STRONG, result);
-        PasswordStrength result2 = meter.meter("abc1!Add");
-        assertEquals(PasswordStrength.STRONG, result2);
+        assertStrength("ab12!@AB", PasswordStrength.STRONG);
+        assertStrength("abc1!Add", PasswordStrength.STRONG);
     }
 
     @DisplayName("[두 번째 테스트] 길이만 8글자 미만이고 나머지 조건은 충족하는 경우")
     @Test
     void meetsOtherCriteria_except_for_Length_Then_Normal(){
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab12!@A");
-        assertEquals(PasswordStrength.NORMAL, result);
+        assertStrength("ab12!@A", PasswordStrength.NORMAL);
     }
 
     @DisplayName("[세 번째 테스트] 숫자를 포함하지 않고 나머지 조건은 충족하는 경우")
     @Test
     void meetsOtherCriteria_except_for_number_Then_Normal(){
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab!@ABqwer");
-        assertEquals(PasswordStrength.NORMAL, result);
+        assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
     }
 }
